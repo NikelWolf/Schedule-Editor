a58be5dc0754edfcb210d04b02f90b53de957531
Nik Sidnev <snive2013@yandex.ru>
2018-03-09 20:17:15 +0300

    Edited cmake file for creating a copy of schedule template in build directory and simple use it in program arguments

--- sources/src/main.cpp
+++ sources/src/main.cpp
@@ -7,6 +7,10 @@ using namespace xlnt;
 using namespace schedule_api;
 
 int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        cerr << "usage: mirea_schedule schedule.xlsx" << endl;
+        return 0;
+    }
     auto wb = open_schedule_file(argv[1]);
     for (auto group: get_all_groups(wb.active_sheet())) {
         cout << group.first << " => (" << group.second.first << ", " << group.second.second << ")\n";
