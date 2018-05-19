2c4eeecb83de501699dd8acde88f3d685cdb1b31
Sonic5015 <khitrov34@gmail.com>
2018-05-19 17:28:46 +0300

    returned qt code in main

--- source/main.cpp
+++ source/main.cpp
@@ -2,8 +2,13 @@
 #include "WMain.h"
 
 int main(int argc, char *argv[]) {
-    Scheduler scheduler{"resources/schedule_template.xlsx"};
-    scheduler.get_groups_non_const_ref().at(0).get_lesson_non_const_ref(1, 1, 1).set_subject_name("tmp");
-    cout << scheduler << endl;
-    scheduler.save_schedule_as("tmp.xlsx");
+
+    QApplication app(argc, argv);
+
+    Scheduler scheduler("resources/schedule_template.xlsx");
+
+    WMain wMain(scheduler);
+    wMain.show();
+
+    return QApplication::exec();
 }
